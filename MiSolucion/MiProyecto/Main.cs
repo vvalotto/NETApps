14ab1d778102733a357cb7db3ae3ed2df1706813
Victor Valotto <vvalotto@gmail.com>
2013-04-04 15:56:43 -0300

    Primer Commit

---
+++ MiSolucion/MiProyecto/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MiProyecto
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ MiSolucion/MiSolucion/Hola.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MiSolucion
+{
+	public class Hola
+	{
+		public Hola ()
+		{
+
+		}
+	}
+}
+
